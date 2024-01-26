f64c0d1528ec3424b99e8c8a5df6c8fc889d427f
Calvin <neocezheng@outlook.com>
2024-01-26 15:01:44 +0800

    modified predict.py

--- src/predict.py
+++ src/predict.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch
 import pandas as pd
 
-def predict(model, imgs):
+def inference(model, imgs):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.eval()
     model.to(device)
-    preds = model(imgs)
-    return torch.argmax(preds, dim = 1)
+    output = model(imgs)
+    return output
