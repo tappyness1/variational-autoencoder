161b9e7daf4e093a5e6b0cef73fc59ac513bc6c0
Calvin <neocezheng@outlook.com>
2024-07-17 07:50:10 +0800

    reworked with checkpointing

--- src/train.py
+++ src/train.py
@@ -25,10 +25,25 @@ def train(train_set, cfg):
         summary(model, (3,224,224))
 
     train_dataloader = DataLoader(train_set, batch_size=cfg['train']['batch_size'], shuffle = True)
-    dataset = cfg['dataset']['dataset']
     
-    for epoch in range(cfg['train']['epochs']):
-        print (f"Epoch {epoch + 1}:")
+    # dataset = cfg['dataset']['dataset']
+    dataset_name = cfg['dataset']['dataset'].lower()
+
+    num_epochs = cfg['train']['epochs']
+
+    if cfg['train']['continue_training']:
+        model_weights = torch.load(cfg['train']['weights_path'],
+                                   map_location=torch.device(device))
+        model.load_state_dict(model_weights)
+
+        # get the saved epochs and continue training from there
+        last_epoch = int(cfg['train']['weights_path'].split("-")[-1].split(".")[0])
+        num_epochs -= last_epoch
+
+    last_epoch = last_epoch if cfg['train']['continue_training'] else 0
+    
+    for epoch in range(last_epoch+1, last_epoch + num_epochs+1):
+        print (f"Epoch {epoch}:")
         # for i in tqdm(range(X.shape[0])):
         overall_loss = []
         with tqdm(train_dataloader) as tepoch:
@@ -44,11 +59,14 @@ def train(train_set, cfg):
                 overall_loss.append(loss.item())
                 tepoch.set_postfix(loss=loss.item())
                 
-            print(f"Epoch {epoch + 1} loss: {sum(overall_loss) / (len(overall_loss)*cfg['train']['batch_size'])}")    
+            print(f"Epoch {epoch + 1} loss: {sum(overall_loss) / (len(overall_loss)*cfg['train']['batch_size'])}")
+
+        if epoch % cfg['train']['save_checkpoint_interval'] == 0:
+            torch.save(model.state_dict(), f"{cfg['save_model_path']}-{dataset_name}-epochs-{epoch}.pt")
+            print (f"Checkpoint saved at epoch {epoch}")
 
     print("training done")
-    dataset = cfg['dataset']['dataset'].lower()
-    torch.save(model.state_dict(), f"{cfg['save_model_path']}_{dataset}.pt")
+    torch.save(model.state_dict(), f"{cfg['save_model_path']}-{dataset_name}.pt")
 
     return model
 
@@ -64,7 +82,10 @@ if __name__ == "__main__":
 
     cfg = {"save_model_path": "model_weights/vae",
            'show_model_summary': False, 
-           'train': {"epochs": 50, 'lr': 0.001, 'weight_decay': 5e-3, 'batch_size': 8},
+           'train': {"epochs": 100, 'lr': 0.001, 'weight_decay': 5e-3, 'batch_size': 8, 
+                     'continue_training': True, 
+                     'save_checkpoint_interval': 5,
+                     'weights_path': "model_weights/vae-flowers102-epochs-50.pt"},
            'dataset': {"dataset": "Flowers102"}}  
 
     # train_set, _ = get_load_data(root = "../data", dataset = cfg['dataset']['dataset'])
